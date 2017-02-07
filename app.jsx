0e1f097ab57d2b9db3a9c613f38a9451728c4430
Evans Gacheru <evans.gacheru@andela.com>
2017-02-07 19:11:25 +0300

    [chore] adds new es5 file

--- app.jsx
+++ app.jsx
@@ -16,7 +16,7 @@ class ComponentTwo extends React.Component{
 	render(){
 		return(
 			<div>
-				<h1 style={{color:'#85E023'}}>Es6 Implementation</h1>
+				<h1 style={{color:'#85E023'}}>Imported Es6 Implementation</h1>
 				<div>
 					<p>Component Two.</p>
 				</div>
@@ -26,27 +26,3 @@ class ComponentTwo extends React.Component{
 }
 ReactDOM.render(<ComponentOne />, document.getElementById('root'))
 
-var ComponentOneA = React.createClass({
-	render(){
-		return(
-			<div>
-				<h1 style={{color:'#F76F00'}}>Es5 Implementation </h1>
-				<div style={{color:'#000'}}>
-					<p>Component One.</p>
-					<ComponentTwoB />
-				</div>
-			</div>
-		)
-	}
-})
-var ComponentTwoB = React.createClass({
-	render(){
-		return(
-			<div>
-				<h1 style={{color:'#F76F00'}}>Es5 Implementation </h1>
-				<div style={{color:'#000'}}><p>Component One.</p></div>
-			</div>
-		)
-	}
-})
-ReactDOM.render(<ComponentOneA />, document.getElementById('body'))
---
+++ es5.jsx
@@ -0,0 +1,24 @@
+var ComponentOneA = React.createClass({
+	render(){
+		return(
+			<div>
+				<h1 style={{color:'#F76F00'}}>Es5 Implementation </h1>
+				<div style={{color:'#000'}}>
+					<p>Component One.</p>
+					<ComponentTwoB />
+				</div>
+			</div>
+		)
+	}
+})
+var ComponentTwoB = React.createClass({
+	render(){
+		return(
+			<div>
+				<h1 style={{color:'#F76F00'}}>Imported Es5 Implementation </h1>
+				<div style={{color:'#000'}}><p>Component One.</p></div>
+			</div>
+		)
+	}
+})
+ReactDOM.render(<ComponentOneA />, document.getElementById('body'))
